a71d3cbbb0e003d7c6738e7696083c73e9ea71f5
KrithikNaralasetty <krithikhi@gmail.com>
2021-12-06 17:51:36 -0800

    Binary tree code changed: Hosts, Switches and Links added

--- HW3_Files/binary_tree.py
+++ HW3_Files/binary_tree.py
@@ -10,9 +10,31 @@ class BinaryTreeTopo( Topo ):
         Topo.__init__( self )
 
 	# Add hosts
-
-	# Add switches
-
-	# Add links
+    hosts = []
+    for i in range(8):
+        s = "host"+str(i+1)
+        hosts.append(self.addHost(s))
+    
+    # Add switches
+    switches = []
+    for i in range(7):
+        s = "switch"+str(i+1)
+        switches.append(self.addSwitch(s))
+	
+    # Add links
+    self.addLink(hosts[0],switches[2])
+    self.addLink(hosts[1],switches[2])
+    self.addLink(hosts[2],switches[3])
+    self.addLink(hosts[3],switches[3])
+    self.addLink(hosts[4],switches[5])
+    self.addLink(hosts[5],switches[5])
+    self.addLink(hosts[6],switches[6])
+    self.addLink(hosts[7],switches[6])
+    self.addLink(switches[2],switches[1])
+    self.addLink(switches[3],switches[1])
+    self.addLink(switches[5],switches[4])
+    self.addLink(switches[6],switches[4])
+    self.addLink(switches[1],switches[0])
+    self.addLink(switches[4],switches[0])
 
 topos = { 'binary_tree': ( lambda: BinaryTreeTopo() ) }
